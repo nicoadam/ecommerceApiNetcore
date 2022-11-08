016a7652cf5f6407b710b75653e1d439a0424a47
unknown <edgar1391@gmail.com>
2022-11-08 10:25:08 -0400

    first commit

---
+++ EcommerceAPI/Ecommerce.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceAPI
+{
+    public class Ecommerce
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; } = string.Empty;
+
+        [StringLength(200)]
+        public string Comments { get; set; } =string .Empty;
+
+        public int EcommerceTypeId { get; set; }
+
+        public EcommerceType? EcommerceType { get; set;}
+
+
+    }
+}
---
+++ EcommerceAPI/EcommerceType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceAPI
+{
+    public class EcommerceType
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string EcommerceName { get; set; } = string.Empty;
+    }
+}
